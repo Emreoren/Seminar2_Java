0b985fa5d8b62cbd7ec8fd670fb2529738d4f09f
krisnawa00 <kristersrudzitiss@gmail.com>
2025-02-21 13:30:09 +0200

    added arrays and cleaned up the code

--- src/seminar2/MainService.java
+++ src/seminar2/MainService.java
@@ -1,44 +1,65 @@
 package seminar2;
 
-
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainService {
+	
+	private static ArrayList<Student> allStudents = new ArrayList<Student>();
+	private static ArrayList<Professor> allProfessors = new ArrayList<Professor>();
+	private static ArrayList<Course> allCourses = new ArrayList<Course>();
+	private static ArrayList<Grade> allGrades = new ArrayList<Grade>();
+
+	
+	
 
 	public static void main(String[] args) {
 
+		
 		Student st1 = new Student();
-		System.out.println(st1.toString());
+		//System.out.println(st1.toString());
 		
 		Student st2 = new Student("John", "Green");
-		System.out.println(st2);
+		//System.out.println(st2);
 		
 		Student st3 = new Student("83907127", "2312511412412");
-		System.out.println(st3);
+		//System.out.println(st3);
 		
 		Student st4 = new Student (null, null);
-		System.out.println(st4);
+		//System.out.println(st4);
+		
+		allStudents.addAll(Arrays.asList(st1, st2, st3, st4));
+		System.out.println(allStudents);
 		
 		Professor p1 = new Professor();
-		System.out.println(p1);
+		//System.out.println(p1);
 
 		Professor p2 = new Professor("Memoli", "Oren", Degree.dr);
-		System.out.println(p2);
+		//System.out.println(p2);
+		
+		allProfessors.addAll(Arrays.asList(p1, p2));
+		System.out.println(allProfessors);
 		
 		Course c1 = new Course();
-		System.out.println(c1);
+		//System.out.println(c1);
 		
 		Course c2 = new Course("JAVA Programming", 6, p2);
-		System.out.println(c2);
+		//System.out.println(c2);
 		
 		Course c3 = new Course("31312312121ghkg1fj1341khbdfdwkjhgf31re73", -4, null);
-		System.out.println(c3);
+		//System.out.println(c3);
+		
+		allCourses.addAll(Arrays.asList(c1, c2, c3));
+		System.out.println(allCourses);
 		
 		Grade g1 = new Grade();
-		System.out.println(g1);
+		//System.out.println(g1);
 		
  		Grade g2 = new Grade(4, st2 ,c2); //st2  is john green c2 is java 
- 		System.out.println(g2);
-				
+ 		//System.out.println(g2);
+
+ 		allGrades.addAll(Arrays.asList(g1, g2));
+		System.out.println(allGrades);
 	}
 
 }
